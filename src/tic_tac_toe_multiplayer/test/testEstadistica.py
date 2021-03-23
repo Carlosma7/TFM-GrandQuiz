47e8162e96eadb2b0cabf0b573fe0134265841c3
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-03-23 16:43:09 +0100

    Close #14 Definición test clase Estadistica

---
+++ src/tic_tac_toe_multiplayer/test/testEstadistica.py
@@ -0,0 +1,22 @@
+import sys
+import os
+sys.path.append(os.path.abspath('./src/tic_tac_toe_multiplayer/core/'))
+
+from estadistica import Estadistica
+import pytest
+from assertpy import assert_that
+
+# Test comparación estadisticas
+def test_compare_estadistica():
+	# Creación de tres objetos estadistica idénticos
+	e1 = Estadistica()
+	e2 = Estadistica()
+	e3 = Estadistica()
+	# Las dos primeras ganan partida y la última pierde
+	e1.add_num_victorias()
+	e2.add_num_victorias()
+	e3.add_num_derrotas()
+	# Comprobar que un objeto estadistica es igual a otro si tienen la misma información
+	assert_that(e1).is_equal_to(e2) # Pasa test
+	# Comprobar que un objeto estadistica es distinto de otro si tienen alguna información distinta
+	assert_that(e1).is_not_equal_to(e3) # Pasa test
