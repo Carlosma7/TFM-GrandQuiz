775df1d40e5cfd4e2b81a259a78db7cb5baedf50
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-03-24 20:32:34 +0100

    #20 Definición clase partida

---
+++ src/questions/core/partida.py
@@ -0,0 +1,37 @@
+from jugador import Jugador
+from pregunta import Pregunta
+from puntuacion import Puntuacion
+from random import randint, choice
+
+# Clase de partida
+class Partida():
+	def __init__(self):
+		self.__jugadores = []
+		self.__puntuaciones = []
+		self.__preguntas = []
+		self.__turno = 0
+		self.__iniciada = False
+		self.__ganador = 0
+
+	# Métodos get/set
+	def get_jugadores(self):
+		return self.__jugadores
+
+	def get_puntuaciones(self):
+		return self.__puntuaciones
+
+	def get_preguntas(self):
+		return self.__preguntas
+
+	def get_turno(self):
+		return self.__turno
+
+	def get_iniciada(self):
+		return self.__iniciada
+
+	def get_ganador(self):
+		return self.__ganador
+
+	# Override método equal
+	def __eq__(self, otra):
+		return (self.__jugadores == otra.get_jugadores()) and (self.__puntuaciones == otra.get_puntuaciones()) and (self.__preguntas == otra.get_preguntas()) and (self.__turno == otra.get_turno()) and (self.__iniciada == otra.get_iniciada()) and (self.__ganador == otra.get_ganador())
