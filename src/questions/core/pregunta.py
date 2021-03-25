fa9a5fdf0a767bdc1c8e857c22196d0360589338
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-03-25 20:22:30 +0100

    #26 Definición to_dict Pregunta

--- src/questions/core/pregunta.py
+++ src/questions/core/pregunta.py
@@ -19,3 +19,7 @@ class Pregunta():
 	# Override método equal
 	def __eq__(self, otra):
 		return (self.__enunciado == otra.get_enunciado()) and (self.__respuestas == otra.get_respuestas()) and (self.__correcta == otra.get_correcta())
+
+	# Método para transformar objeto en un dict
+	def to_dict(self):
+		return {'enunciado': self.get_enunciado(), 'respuestas': self.get_respuestas(), 'correcta': self.get_correcta()}
