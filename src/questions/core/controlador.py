7829323ed30509e10d2d9529fb98e7d08e621418
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-03-25 02:30:09 +0100

    #22 Definición clase controlador y método crear jugador

---
+++ src/questions/core/controlador.py
@@ -0,0 +1,28 @@
+from partida import Partida
+from pregunta import Pregunta
+
+from typing import List
+
+# Clase controladora
+class Controlador():
+	# Listas de entidades
+	jugadores = List[Jugador] = []
+	partidas = List[Partida] = []
+
+	# Crear jugador
+	def crear_jugador(self, jugador: Jugador):
+		# Comprobar que no existe un jugador con el mismo nick de Telegram
+		jug = [j for j in self.jugadores if j.get_nombre_usuario() == jugador.get_nombre_usuario()]
+		no_encontrado = (len(jug) == 0)
+
+		# Si no existe
+		if no_encontrado:
+			# Se comprueba una edad válida
+			if jugador.get_edad() in range(6, 99):
+				# Se añade el jugador
+				self.jugadores.append(jugador)
+			else:
+				raise AgeNotValidError('La edad del jugador no es válida.')
+		else:
+			raise ExistingGameError('Ya existe una partida en el grupo.')
+
