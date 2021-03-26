64d6b224e06ba78fc212bbd5bad598935634107a
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-03-26 21:19:04 +0100

    Añade ID pregunta

--- src/questions/core/pregunta.py
+++ src/questions/core/pregunta.py
@@ -1,12 +1,16 @@
 from typing import List
 # Clase de pregunta
 class Pregunta():
-	def __init__(self, enunciado: str, respuestas: List[str], correcta: int):
+	def __init__(self, id: int, enunciado: str, respuestas: List[str], correcta: int):
+		self.__id = id
 		self.__enunciado = enunciado
 		self.__respuestas = respuestas
 		self.__correcta = correcta
 
 	# Métodos get/set
+	def get_id(self):
+		return self.__id
+
 	def get_enunciado(self):
 		return self.__enunciado
 
@@ -18,8 +22,8 @@ class Pregunta():
 
 	# Override método equal
 	def __eq__(self, otra):
-		return (self.__enunciado == otra.get_enunciado()) and (self.__respuestas == otra.get_respuestas()) and (self.__correcta == otra.get_correcta())
+		return (self.__id == otra.get_id()) and (self.__enunciado == otra.get_enunciado()) and (self.__respuestas == otra.get_respuestas()) and (self.__correcta == otra.get_correcta())
 
 	# Método para transformar objeto en un dict
 	def to_dict(self):
-		return {'enunciado': self.get_enunciado(), 'respuestas': self.get_respuestas(), 'correcta': self.get_correcta()}
+		return {'id': self.get_id(), 'enunciado': self.get_enunciado(), 'respuestas': self.get_respuestas(), 'correcta': self.get_correcta()}
