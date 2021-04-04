00809c429d53961c4e7236814094a3d28a936b65
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-04-05 01:04:49 +0200

    #31 Definición método registrar jugador

--- src/grandquiz/core/controlador.py
+++ src/grandquiz/core/controlador.py
@@ -1,5 +1,6 @@
 from jugador import Jugador
 
+import os
 from dotenv import load_dotenv
 import pymongo
 
@@ -11,10 +12,23 @@ MONGO_TOKEN = os.getenv('MONGO')
 # Define a client
 client = pymongo.MongoClient(MONGO_TOKEN, serverSelectionTimeoutMS = 2000)
 # Define the database
-database = mongo.GrandQuiz
+database = client.GrandQuiz
 
 # Clase controlador
 class Controlador():
 
 	# Definir la conexión a MongoDB Atlas
 	mongo = database
+
+	# Registrar jugador
+	def registrar_jugador(self, jugador: Jugador):
+		# Comprobar que no existe un jugador con el mismo nick de Telegram
+		jug = self.mongo.jugadores.find({'nombre_usuario': jugador.get_nombre_usuario()})
+		no_encontrado = (jug.count() == 0)
+
+		# Si no existe
+		if no_encontrado:
+			# Se añade el jugador
+			self.mongo.jugadores.insert_one(jugador.to_dict())
+		else:
+			raise ValueError('Ya estás registrado.')
