90aa28e9b32bad554d8aeb2e7ebf98b5f28d3213
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-04-04 22:33:38 +0200

    Close #29 Definición test clase Jugador

---
+++ src/grandquiz/test/testJugador.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.abspath('./src/grandquiz/core/'))
+
+from jugador import Jugador
+import pytest
+from assertpy import assert_that
+
+# Test comparación jugador
+def test_compare_jugador():
+	# Creación de dos jugadores idénticos
+	j1 = Jugador("Carlosma7", "Carlos")
+	j2 = Jugador("Carlosma7", "Carlos")
+	# Creación de un jugador distinto
+	j3 = Jugador("Carlosma7", "Fernando")
+	# Comprobar que un jugador es igual a otro si tienen la misma información
+	assert_that(j1).is_equal_to(j2) # Pasa test
+	# Comprobar que un jugador es distinto de otro si tienen alguna información distinta
+	assert_that(j1).is_not_equal_to(j3) # Pasa test
