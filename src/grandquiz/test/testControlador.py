1a61d8a352346a5982d8c29b05591f9b920a4da9
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-04-06 19:44:28 +0200

    #32 Definición test cambiar avatar

--- src/grandquiz/test/testControlador.py
+++ src/grandquiz/test/testControlador.py
@@ -20,3 +20,22 @@ def test_registrar_jugador_controlador():
 	assert_that(c.mongo.jugadores.find({'nombre_usuario':j1.get_nombre_usuario()}).count()).is_equal_to(1)
 	# Borrar jugador de test
 	c.mongo.jugadores.delete_one({'nombre_usuario':j1.get_nombre_usuario()})
+
+# Test de cambiar avatar de jugador
+def test_cambiar_avatar_controlador():
+	# Crear controlador
+	c = Controlador()
+	# Crear objeto jugador
+	j1 = Jugador("Test2", "Test")
+	# Crear administrador
+	c.registrar_jugador(j1)
+	# Comprobar que el jugador no tiene avatar
+	assert_that(j1.get_avatar()).is_equal_to("")
+	# Cambiar avatar usuario
+	c.cambiar_avatar(j1.get_nombre_usuario(), "avatar")
+	# Obtener el jugador de la BD
+	j2 = c.mongo.jugadores.find_one({'nombre_usuario':j1.get_nombre_usuario()})
+	# Comprobar que el avatar ha cambiado
+	assert_that(j2.get('avatar')).is_not_equal_to("")
+	# Borrar jugador de test
+	c.mongo.jugadores.delete_one({'nombre_usuario':j1.get_nombre_usuario()})
