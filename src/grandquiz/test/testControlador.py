ab3585477d60138d55c47382b8e30c6b3448ae66
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-06-18 01:24:03 +0200

    #32 Definición test comprobar medalla duelo

--- src/grandquiz/test/testControlador.py
+++ src/grandquiz/test/testControlador.py
@@ -1662,3 +1662,38 @@ def test_add_estadisticas_duelo_controlador():
 	# Borrar duelo de test
 	c.mongo.duelos.delete_one({'chat':d1.get_chat()})
 	c.mongo.duelos.delete_one({'chat':d2.get_chat()})
+
+# Test de comprobar medallas de un jugador en un duelo
+def test_comprobar_medalla_duelo_controlador():
+	# Crear controlador
+	c = Controlador()
+	# Crear objetos jugador
+	j1 = Jugador("Test91", "Test91")
+	j1.set_edad("edad0")
+	j2 = Jugador("Test92", "Test92")
+	j2.set_edad("edad1")
+	# Crear jugadores
+	c.registrar_jugador(j1)
+	c.registrar_jugador(j2)
+	# Crear objeto duelo
+	d1 = Duelo("Test11", j1.get_nombre_usuario())
+	d2 = Duelo("Test12", j2.get_nombre_usuario())
+	# Crear duelo
+	c.crear_duelo(d1, j1.get_nombre_usuario())
+	turno, ava_turno, pregunta, categoria, chat1, chat2 = c.crear_duelo(d2, j2.get_nombre_usuario())
+	# Comprobar que el equipo no tiene la medalla de la categoria actual
+	medalla, categoria = c.comprobar_medalla_duelo(chat1)
+	# Comprobar que es falso
+	assert_that(medalla).is_false()
+	# Borrar jugador de test
+	c.mongo.jugadores.delete_one({'nombre_usuario':j1.get_nombre_usuario()})
+	c.mongo.jugadores.delete_one({'nombre_usuario':j2.get_nombre_usuario()})
+	# Borrar estadistica de test
+	c.mongo.estadisticas.delete_one({'nombre_usuario':j1.get_nombre_usuario()})
+	c.mongo.estadisticas.delete_one({'nombre_usuario':j2.get_nombre_usuario()})
+	# Borrar logros de test
+	c.mongo.logros.delete_one({'nombre_usuario':j1.get_nombre_usuario()})
+	c.mongo.logros.delete_one({'nombre_usuario':j2.get_nombre_usuario()})
+	# Borrar duelo de test
+	c.mongo.duelos.delete_one({'chat':d1.get_chat()})
+	c.mongo.duelos.delete_one({'chat':d2.get_chat()})
