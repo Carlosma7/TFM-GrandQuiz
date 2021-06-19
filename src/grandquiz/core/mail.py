088e626b85fedaac59c39130f4a77ac5a3b3b49d
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-06-19 23:59:50 +0200

    Fix indentation

--- src/grandquiz/core/mail.py
+++ src/grandquiz/core/mail.py
@@ -11,36 +11,35 @@ PASS_GMAIL = os.getenv('PASS_GMAIL')
 
 # Clase de mail
 class Mail():
-  def __init__(self, destinatario: str):
-    self.__emisor = 'grandquizbot@gmail.com'
-    self.__destinatario = destinatario
-    self.__message = ""
+	def __init__(self, destinatario: str):
+		self.__emisor = 'grandquizbot@gmail.com'
+		self.__destinatario = destinatario
+		self.__message = ""
 
-  # Métodos
-  def set_mail(self, jugador: str, tipo: int):
-    if tipo == 1:
-    # El correo es de registro
-    self.__message = MIMEMultipart("alternative")
-    self.__message["From"] = self.__emisor
-    self.__message["To"] = self.__destinatario
+	# Métodos
+	def set_mail(self, jugador: str, tipo: int):
+		# El correo es de registro
+		self.__message = MIMEMultipart("alternative")
+		self.__message["From"] = self.__emisor
+		self.__message["To"] = self.__destinatario
 
-    if tipo == 1:
-      self.__message["Subject"] = "Bienvenida a GrandQuiz"
-      html = open("src/grandquiz/core/registro.html")
-    else:
-      self.__message["Subject"] = "Notificación inactividad en GrandQuiz"
-      html = open("src/grandquiz/core/inactividad.html")
+		if tipo == 1:
+			self.__message["Subject"] = "Bienvenida a GrandQuiz"
+			html = open("src/grandquiz/core/registro.html")
+		else:
+			self.__message["Subject"] = "Notificación inactividad en GrandQuiz"
+			html = open("src/grandquiz/core/inactividad.html")
 
-    html = html.read()
-    html = html.replace("Jugador", jugador)
+		html = html.read()
+		html = html.replace("Jugador", jugador)
 
-    cuerpo = MIMEText(html, "html")
-    self.__message.attach(cuerpo)
+		cuerpo = MIMEText(html, "html")
+		self.__message.attach(cuerpo)
 
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(self.__emisor, PASS_GMAIL)
-        server.sendmail(
-            self.__emisor, self.__destinatario, self.__message.as_string()
-        )
+		# Create secure connection with server and send email
+		context = ssl.create_default_context()
+		with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+			server.login(self.__emisor, PASS_GMAIL)
+			server.sendmail(
+				self.__emisor, self.__destinatario, self.__message.as_string()
+			)
