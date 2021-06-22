e0102ebfcd36dd6590ac5acf4cf654b131189988
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-06-22 13:12:20 +0200

    Fix import types

--- src/grandquiz/core/markups.py
+++ src/grandquiz/core/markups.py
@@ -1,25 +1,24 @@
 import telebot
 from variables import *
 from typing import List
-from telebot import types
 from pregunta import Pregunta
 from desafio import Desafio
 
 # Markups Bot API para elegir avatar
 def markup_avatar():
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 5)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 5)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton("\U0001f466\U0001f3fb", callback_data="av0"))
-	bt2 = (types.InlineKeyboardButton("\U0001f467\U0001f3fb", callback_data="av1"))
-	bt3 = (types.InlineKeyboardButton("\U0001f9d1\U0001f3fb\u200D\U0001f9b3", callback_data="av2"))
-	bt4 = (types.InlineKeyboardButton("\U0001f468\U0001f3fb\u200D\U0001f9b3", callback_data="av3"))
-	bt5 = (types.InlineKeyboardButton("\U0001f916", callback_data="av4"))
-	bt6 = (types.InlineKeyboardButton("\U0001f47d", callback_data="av5"))
-	bt7 = (types.InlineKeyboardButton("\U0001f435", callback_data="av6"))
-	bt8 = (types.InlineKeyboardButton("\U0001f438", callback_data="av7"))
-	bt9 = (types.InlineKeyboardButton("\U0001f920", callback_data="av8"))
-	bt10 = (types.InlineKeyboardButton("\U0001f60e", callback_data="av9"))
+	bt1 = (telebot.types.InlineKeyboardButton("\U0001f466\U0001f3fb", callback_data="av0"))
+	bt2 = (telebot.types.InlineKeyboardButton("\U0001f467\U0001f3fb", callback_data="av1"))
+	bt3 = (telebot.types.InlineKeyboardButton("\U0001f9d1\U0001f3fb\u200D\U0001f9b3", callback_data="av2"))
+	bt4 = (telebot.types.InlineKeyboardButton("\U0001f468\U0001f3fb\u200D\U0001f9b3", callback_data="av3"))
+	bt5 = (telebot.types.InlineKeyboardButton("\U0001f916", callback_data="av4"))
+	bt6 = (telebot.types.InlineKeyboardButton("\U0001f47d", callback_data="av5"))
+	bt7 = (telebot.types.InlineKeyboardButton("\U0001f435", callback_data="av6"))
+	bt8 = (telebot.types.InlineKeyboardButton("\U0001f438", callback_data="av7"))
+	bt9 = (telebot.types.InlineKeyboardButton("\U0001f920", callback_data="av8"))
+	bt10 = (telebot.types.InlineKeyboardButton("\U0001f60e", callback_data="av9"))
 	markup.add(bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt10)
 
 	return markup
@@ -27,11 +26,11 @@ def markup_avatar():
 # Markups Bot API para elegir rango de edad
 def markup_edad():
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 1)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton("Menor de 30 años", callback_data="edad0"))
-	bt2 = (types.InlineKeyboardButton("Entre 30 y 60 años", callback_data="edad1"))
-	bt3 = (types.InlineKeyboardButton("Mayor de 60 años", callback_data="edad2"))
+	bt1 = (telebot.types.InlineKeyboardButton("Menor de 30 años", callback_data="edad0"))
+	bt2 = (telebot.types.InlineKeyboardButton("Entre 30 y 60 años", callback_data="edad1"))
+	bt3 = (telebot.types.InlineKeyboardButton("Mayor de 60 años", callback_data="edad2"))
 	markup.add(bt1, bt2, bt3)
 
 	return markup
@@ -39,10 +38,10 @@ def markup_edad():
 # Markups Bot API para elegir equipo al que unirse
 def markup_equipos(equipos_disponibles):
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 1)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton(f"Equipo {colores_equipos.get('rojo')}", callback_data="eq0"))
-	bt2 = (types.InlineKeyboardButton(f"Equipo {colores_equipos.get('azul')}", callback_data="eq1"))
+	bt1 = (telebot.types.InlineKeyboardButton(f"Equipo {colores_equipos.get('rojo')}", callback_data="eq0"))
+	bt2 = (telebot.types.InlineKeyboardButton(f"Equipo {colores_equipos.get('azul')}", callback_data="eq1"))
 	# Comprobar equipos disponibles
 	if len(equipos_disponibles) == 2:
 		# Se añaden botones para los dos equipos
@@ -64,12 +63,12 @@ def markup_equipos(equipos_disponibles):
 # Markups Bot API para responder a una pregunta
 def markup_respuestas(pregunta: Pregunta):
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 1)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton(pregunta.get_respuestas()[0], callback_data="pr1"))
-	bt2 = (types.InlineKeyboardButton(pregunta.get_respuestas()[1], callback_data="pr2"))
-	bt3 = (types.InlineKeyboardButton(pregunta.get_respuestas()[2], callback_data="pr3"))
-	bt4 = (types.InlineKeyboardButton(pregunta.get_respuestas()[3], callback_data="pr4"))
+	bt1 = (telebot.types.InlineKeyboardButton(pregunta.get_respuestas()[0], callback_data="pr1"))
+	bt2 = (telebot.types.InlineKeyboardButton(pregunta.get_respuestas()[1], callback_data="pr2"))
+	bt3 = (telebot.types.InlineKeyboardButton(pregunta.get_respuestas()[2], callback_data="pr3"))
+	bt4 = (telebot.types.InlineKeyboardButton(pregunta.get_respuestas()[3], callback_data="pr4"))
 	markup.add(bt1, bt2, bt3, bt4)
 
 	return markup
@@ -81,10 +80,10 @@ def markup_respuestas_quizzies(pregunta: Pregunta, respuestas: List[str]):
 		return markup_respuestas(pregunta)
 	else:
 		# Keyboard
-		markup = types.InlineKeyboardMarkup(row_width = 1)
+		markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 		# Buttons
-		bt1 = (types.InlineKeyboardButton(pregunta.get_respuestas()[int(respuestas[0]) - 1], callback_data=f"pr{respuestas[0]}"))
-		bt2 = (types.InlineKeyboardButton(pregunta.get_respuestas()[int(respuestas[1]) - 1], callback_data=f"pr{respuestas[1]}"))
+		bt1 = (telebot.types.InlineKeyboardButton(pregunta.get_respuestas()[int(respuestas[0]) - 1], callback_data=f"pr{respuestas[0]}"))
+		bt2 = (telebot.types.InlineKeyboardButton(pregunta.get_respuestas()[int(respuestas[1]) - 1], callback_data=f"pr{respuestas[1]}"))
 		markup.add(bt1, bt2)
 
 		return markup
@@ -92,11 +91,11 @@ def markup_respuestas_quizzies(pregunta: Pregunta, respuestas: List[str]):
 # Markups Bot API para mostrar quizzies
 def markup_quizzies(quizzies_jugador: dict):
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 1)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton(f"{quizzies.get('1')} - {quizzies_jugador.get('1')}", callback_data="qui1"))
-	bt2 = (types.InlineKeyboardButton(f"{quizzies.get('2')} - {quizzies_jugador.get('2')}", callback_data="qui2"))
-	bt3 = (types.InlineKeyboardButton(f"{quizzies.get('3')} - {quizzies_jugador.get('3')}", callback_data="qui3"))
+	bt1 = (telebot.types.InlineKeyboardButton(f"{quizzies.get('1')} - {quizzies_jugador.get('1')}", callback_data="qui1"))
+	bt2 = (telebot.types.InlineKeyboardButton(f"{quizzies.get('2')} - {quizzies_jugador.get('2')}", callback_data="qui2"))
+	bt3 = (telebot.types.InlineKeyboardButton(f"{quizzies.get('3')} - {quizzies_jugador.get('3')}", callback_data="qui3"))
 	# Comprobar los quizzies disponibles
 	if quizzies_jugador.get('1') != 0:
 		if quizzies_jugador.get('2') != 0:
@@ -124,12 +123,12 @@ def markup_quizzies(quizzies_jugador: dict):
 # Markups Bot API para responder a un desafío
 def markup_desafio(desafio: Desafio):
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 1)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton(desafio.get_respuestas()[0], callback_data="des1"))
-	bt2 = (types.InlineKeyboardButton(desafio.get_respuestas()[1], callback_data="des2"))
-	bt3 = (types.InlineKeyboardButton(desafio.get_respuestas()[2], callback_data="des3"))
-	bt4 = (types.InlineKeyboardButton(desafio.get_respuestas()[3], callback_data="des4"))
+	bt1 = (telebot.types.InlineKeyboardButton(desafio.get_respuestas()[0], callback_data="des1"))
+	bt2 = (telebot.types.InlineKeyboardButton(desafio.get_respuestas()[1], callback_data="des2"))
+	bt3 = (telebot.types.InlineKeyboardButton(desafio.get_respuestas()[2], callback_data="des3"))
+	bt4 = (telebot.types.InlineKeyboardButton(desafio.get_respuestas()[3], callback_data="des4"))
 	markup.add(bt1, bt2, bt3, bt4)
 
 	return markup
@@ -137,10 +136,10 @@ def markup_desafio(desafio: Desafio):
 # Markups Bot API para confirmar baja
 def markup_baja():
 	# Keyboard
-	markup = types.InlineKeyboardMarkup(row_width = 1)
+	markup = telebot.types.InlineKeyboardMarkup(row_width = 1)
 	# Buttons
-	bt1 = (types.InlineKeyboardButton("Sí", callback_data="baja0"))
-	bt2 = (types.InlineKeyboardButton("No", callback_data="baja1"))
+	bt1 = (telebot.types.InlineKeyboardButton("Sí", callback_data="baja0"))
+	bt2 = (telebot.types.InlineKeyboardButton("No", callback_data="baja1"))
 	markup.add(bt1, bt2)
 
 	return markup
